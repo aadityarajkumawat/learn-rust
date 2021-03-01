24050925c6dd511db6950f2ea7e08b7b1c4f2432
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-01 23:11:10 +0530

    learnt method syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,17 +4,30 @@ struct User {
     sign_in_count: u64,
     active: bool
 }
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+// This is called implementation block
+// I think this is something similar
+// to classes and self is like `this`
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
 fn main() {
-    /**
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someuser123"),
@@ -33,8 +46,6 @@ fn main() {
     let black = Color(0, 0, 0); // black color
     let origin = Point(0, 0, 0); // relative center
 
-    println!("{}", black.0);
-    */
     let width1 = 30;
     let height1 = 50;
 
@@ -44,6 +55,24 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    let r1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let r2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let r3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", r1.can_hold(&r2));
+    println!("Can rect1 hold rect2? {}", r1.can_hold(&r3));
 
     //println!("The area of rectangle is {} square pixels.", area(&rect1));
     println!("The area of rectangle is {:#?} square pixels.", rect2);
