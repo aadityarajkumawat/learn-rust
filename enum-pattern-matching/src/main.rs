5655ccd00373ef7a5bfa5e1f873b3f0ed6d1d43e
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-02 23:57:27 +0530

    learnt option enum

--- enum-pattern-matching/src/main.rs
+++ enum-pattern-matching/src/main.rs
@@ -5,15 +5,41 @@ enum IpAddrKind {
     V6,
 }
 
-struct IpAddr {
+// This allows us eliminating
+// the struct after it
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct IpAddr1 {
     kind: IpAddrKind,
     address: String,
+}
+
+impl Message {
+    fn call(&self) {
+        // call
+    }
+}
+
+enum Option<T> {
+    Some(T),
+    None,
 }
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
+    /**
     let home = IpAddr {
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
@@ -23,9 +49,23 @@ fn main() {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
+    */
 
-    println!("{}", home.address);
+    //let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    println!("Hello, world!");
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let another_number = Some(10);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let ii15 = some_number + another_number;
+
+    println!("{}", ii15);
 }
 
