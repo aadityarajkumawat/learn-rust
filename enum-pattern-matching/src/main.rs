a72bb1b86c61b038a91411a41f5aeebe706f7616
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-03 17:28:18 +0530

    completed match and if let

--- enum-pattern-matching/src/main.rs
+++ enum-pattern-matching/src/main.rs
@@ -36,6 +36,42 @@ enum Option<T> {
     None,
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Option::None => Option::None,
+        Option::Some(i) => Option::Some(i + 1),
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
@@ -63,6 +99,23 @@ fn main() {
 
     let absent_number: Option<i32> = Option::None;
 
-    println!("{:?}", absent_number);
+    //println!("{:?}", absent_number);
+
+    let k = value_in_cents(Coin::Quarter(UsState::Alaska));
+    //println!("{}", k);
+    
+    let five = Option::Some(5);
+    let six = plus_one(five);
+
+    let none = plus_one(Option::None);
+
+    let some_u8_value = Option::Some(3);
+    //match some_u8_value {
+      //  Some(3) => println!("three"),
+      //  _ => (),
+    //}
+    if let Option::Some(3) = some_u8_value {
+        println!("three");
+    }
 }
 
--- enum-pattern-matching/src/matchh.rs
+++ enum-pattern-matching/src/matchh.rs
@@ -1,15 +1,34 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+fn main() {
+    let k = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{}", k);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!");
+            25
+        },
     }
 }
+
+
