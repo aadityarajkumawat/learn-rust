4dbd4f35d78a4022e32c63804c4fb10d09b94436
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-03-03 14:32:24 +0530

    trying to learn Option enum

--- enum-pattern-matching/src/main.rs
+++ enum-pattern-matching/src/main.rs
@@ -30,6 +30,7 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
 enum Option<T> {
     Some(T),
     None,
@@ -39,17 +40,15 @@ fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    /**
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    //let home = IpAddr {
+      //  kind: IpAddrKind::V4,
+      //  address: String::from("127.0.0.1"),
+    //};
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-    */
+   // let loopback = IpAddr {
+     //   kind: IpAddrKind::V6,
+     //   address: String::from("::1"),
+    //};
 
     //let home = IpAddr::V4(String::from("127.0.0.1"));
     let home = IpAddr::V4(127, 0, 0, 1);
@@ -62,10 +61,8 @@ fn main() {
     let another_number = Some(10);
     let some_string = Some("a string");
 
-    let absent_number: Option<i32> = None;
+    let absent_number: Option<i32> = Option::None;
 
-    let ii15 = some_number + another_number;
-
-    println!("{}", ii15);
+    println!("{:?}", absent_number);
 }
 
